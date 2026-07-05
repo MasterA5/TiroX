f44c955417fee8a706fed3dc06878c101ff2bea7
MasterA5 <agustin090806090806@gmail.com>
2026-07-04 23:10:58 -0300

    refactor last result card in a spearated controls

--- src/components/LastResultCard.py
+++ src/components/LastResultCard.py
@@ -4,7 +4,6 @@ from flet import (
     Container,
     FontWeight,
     Icon,
-    Icons,
     MainAxisAlignment,
     Row,
     Text,
@@ -12,10 +11,53 @@ from flet import (
     TextStyle,
 )
 
+from utils.formater import Formater
+from utils.get_color_by_value import get_color_by_value
+from utils.get_icon_by_value import get_icon_by_value
+
+
+class LastResultCardTag(Container):
+    def __init__(self, value: float | int | str):
+        super().__init__()
+        self.value = value
+        self.tag_text, self.bgcolor, self.text_color = get_color_by_value(
+            float(self.value),
+            bg_custom_colors=[
+                Colors.LIGHT_GREEN_ACCENT_700,
+                Colors.AMBER_600,
+                Colors.RED,
+            ],
+            txt_custom_colors=[
+                Colors.LIGHT_GREEN_50,
+                Colors.YELLOW_100,
+                Colors.RED_50,
+            ],
+        )
+        self.icon = get_icon_by_value(float(self.value))
+        self.content = Row(
+            controls=[
+                Icon(
+                    self.icon,
+                    size=15,
+                    color=self.text_color,
+                ),
+                Text(
+                    self.tag_text,
+                    size=13,
+                    color=self.text_color,
+                ),
+            ],
+            tight=True,
+            spacing=5,
+        )
+        self.padding = 5
+        self.border_radius = 20
+
 
 class LastResultCard(Container):
     def __init__(self, hormone: str, value: float, date: str):
         super().__init__()
+        self.value = value
         self.content = Column(
             controls=[
                 Text(
@@ -43,30 +85,10 @@ class LastResultCard(Container):
                 ),
                 Row(
                     controls=[
-                        Container(
-                            content=Row(
-                                controls=[
-                                    Icon(
-                                        Icons.CHECK,
-                                        size=13,
-                                        color=Colors.WHITE,
-                                    ),
-                                    Text(
-                                        "Normal",
-                                        size=13,
-                                        color=Colors.WHITE,
-                                    ),
-                                ],
-                                tight=True,
-                                spacing=5,
-                            ),
-                            padding=5,
-                            bgcolor=Colors.GREEN_300,
-                            border_radius=20,
-                        ),
+                        LastResultCardTag(self.value),
                         Container(
                             content=Text(
-                                date,
+                                Formater.format_datetime(str(date), legible=True),
                                 color=Colors.WHITE,
                             )
                         ),
